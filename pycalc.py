3a0276e9efc83ec0571648f590cfb51ce0bcc33e
Thiago Alves Bezerra <22500657@me.uvsq.fr>
2026-02-26 12:03:48 +0100

    ma base

---
+++ pycalc.py
@@ -0,0 +1,5 @@
+def calculator():
+	valeur1 = int(input("Entrez la première valeur : \n"))
+	valeur2 = int(input("Entrez la deuxième valeur : \n"))
+	modeOp = input("Entrez le mode d'opération : \n s(Somme) \n diff(différance \n m(multiplication) \n d(division \n choix:")
+
