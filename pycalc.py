be038eda06e0121cb469fa44397ad6bc3352d2b9
Thiago Alves Bezerra <22500657@me.uvsq.fr>
2026-02-26 12:06:54 +0100

    ajout Somme

--- pycalc.py
+++ pycalc.py
@@ -3,3 +3,5 @@ def calculator():
 	valeur2 = int(input("Entrez la deuxième valeur : \n"))
 	modeOp = input("Entrez le mode d'opération : \n s(Somme) \n diff(différance \n m(multiplication) \n d(division \n choix:")
 
+	if modeOp == "s"
+		print("résultat :", valeur1 + valeur2)
